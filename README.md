25451ce6fceb1a85ec539988ea0ba9719fc06e7e
cristyanlopesrlz <cristyan@rlz.com.br>
2019-12-13 16:06:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ua4ybu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ua4ybu)
---
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-bar',
+  templateUrl: './bottom-bar.component.html',
+  styleUrls: ['./bottom-bar.component.css']
+})
+export class BottomBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
